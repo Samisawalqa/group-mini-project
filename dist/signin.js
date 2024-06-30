93b7b64d74f0a080a251d1d3f9e89e56b3e08e0e
RGD97 <rgd.alshobaki@gmail.com>
2024-06-30 17:31:12 +0300

    add

--- dist/signin.js
+++ dist/signin.js
@@ -1,24 +1,34 @@
-
-
-
-
-localStorage.setItem('users', JSON.stringify([
-    { email: 'user1@example.com', password: 'password123', name: 'John Doe' },
-    { email: 'user2@example.com', password: 'password456', name: 'Jane Smith' }
-]));
-
-document.getElementById('signinForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    const email = document.getElementById('signinEmail').value;
-    const password = document.getElementById('signinPassword').value;
-    const users = JSON.parse(localStorage.getItem('users'));
-    const user = users.find(user => user.email === email && user.password === password);
-    if (user) {
-        localStorage.setItem('isSignedIn', true);
-        localStorage.setItem('signedInUser', JSON.stringify({ name: user.name, email: user.email }));
-        alert('Sign in successful !');
-        window.location.href = 'dashboard.html';
-    } else {
-        alert('Incorrect email or password.');
-    }
-});
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+let users = [{ email: 'abdallah.awaysheh@taskbuddy.com', password: '123', name: 'AbdallahAwaysheh' }];
+            // localStorage.clear()
+            document.getElementById('signupForm').addEventListener('submit', function(event) {
+                event.preventDefault();
+                let id = Math.floor(Math.random() * 99);
+                    localStorage.getItem('users');
+                    users.push({
+                        id: id,
+                        activeStatus: false,
+                        name: firstName.value,
+                        email: email.value,
+                        password: password.value
+                    });
+                    localStorage.setItem('users', JSON.stringify(users));
+            });
+            document.getElementById('signinForm').addEventListener('submit', function(event) {
+                event.preventDefault(); 
+                const email = document.getElementById('signinEmail').value;
+                const password = document.getElementById('signinPassword').value;
+                const users = JSON.parse(localStorage.getItem('users'));
+                const user = users.find(user => user.email === email && user.password === password);
+                if (user) {
+                    localStorage.setItem('activeStatus', true);
+                    localStorage.setItem('signedInUser', JSON.stringify({ name: user.name, email: user.email }));
+                    // alert('Sign in successful !');
+                    window.location.href = 'dashboard.html';
+                } else {
+                    alert('Incorrect email or password.');
+                }
+            });
