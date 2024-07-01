f9331764b847508aa37d9daa42be468255d06667
RGD97 <rgd.alshobaki@gmail.com>
2024-07-01 13:04:10 +0300

    update

--- dist/main.js
+++ dist/main.js
@@ -92,82 +92,145 @@ let show = document.getElementById("to_add_proj");
             const signedInUser= JSON.parse(localStorage.getItem('signedInUser'));
             signedInUser.project = {projectName:projectName,projectDescription:projectDescription};
             localStorage.setItem("signedInUser",JSON.stringify(signedInUser));
-            document.getElementById('to_add_proj').innerHTML+=`<div class="card-header">
-                                <i class="fas fa-table me-1"></i>
-                                <div>"${signedInUser.project['projectName']}"</div> 
-                                <div>"${signedInUser.project['projectDescription']}"</div> 
-
+            document.getElementById('to_add_proj').innerHTML+=`<div class="card-header d-flex justify-content-between align-items-center">
+                                <div class="d-flex align-items-center">
+                                    <i class="fas fa-table me-1"></i>
+                                    <div>${signedInUser.project.projectName}</div>
+                                    <div>${signedInUser.project.projectDescription}</div>
+                                </div>
+                                <div class="bg-danger rounded p-2 btn ">
+                                    <span class="text-light" id="removeProject_btn" > - RemoveProject</span>
+                                    </div>
                             </div>
                             <div id="p_cards" class="card-body">
                                 <div class="card" style="width: 18rem;">
-                                    <div class="card-header">
-                                        <div class="head_pTasks">
-                                            <div><strong>To Do</strong></div>
+                                    <div class="card-header card-header d-flex justify-content-between">
+                                            <span>To Do</span>
                                             <i class="fa-solid fa-plus" style="color: #769fcd;"></i>
-                                        </div>
                                     </div>
                                     <ul class="list-group list-group-flush">
-                                        <div class="taskItem">
-                                            
-                                            
-                                            
+                                        <div class="taskItem ">
+                                            <li class="list-group-item card-header d-flex justify-content-between align-items-center">
+                                                "Task 1"
+                                                <div>
+                                                    <i class="fa-solid fa-pen-to-square"></i>
+                                                    <i class="fa-solid fa-trash"></i>
+                                                </div>
+                                            </li>
                                         </div>
                                         <div>
-                                            
+                                            <li
+                                                class="list-group-item card-header d-flex justify-content-between align-items-center">
+                                                "Task 1"
+                                                <div>
+                                                    <i class="fa-solid fa-pen-to-square"></i>
+                                                    <i class="fa-solid fa-trash"></i>
+                                                </div>
+
+                                            </li>
 
                                         </div>
                                         <div>
-                                            
+                                            <li
+                                                class="list-group-item card-header d-flex justify-content-between align-items-center">
+                                                "Task 1"
+                                                <div>
+                                                    <i class="fa-solid fa-pen-to-square"></i>
+                                                    <i class="fa-solid fa-trash"></i>
+                                                </div>
+
+                                            </li>
                                         </div>
                                     </ul>
-                                  </div>
-                                  <div class="card" style="width: 18rem;">
-                                    <div class="card-header">
-                                        <div>
-                                            <strong>In progress</strong>
+                                </div>
+                                <div class="card" style="width: 18rem;">
+                                    <div class="card-header d-flex justify-content-between">
+                                        
+                                            <span>In progress</span>
                                             <i class="fa-solid fa-plus" style="color: #769fcd;"></i>
-                                        </div>
-                                    </div>
-                                    <ul class="list-group list-group-flush">
-                                        <div>
-                                            
-
-                                        </div>
-                                        <div>
-                                           
-
-                                        </div>
-                                        <div>
-                                            
-
-                                        </div>
-                                    </ul>
-                                  </div>
-                                  <div class="card" style="width: 18rem;">
-                                    <div class="card-header">
-                                        <div>
-                                            <strong>Compleated</strong>
-                                            <i class="fa-solid fa-plus" style="color: #769fcd;"></i>
-
-                                        </div>
+                                        
                                     </div>
                                     <ul class="list-group list-group-flush">
                                         <div>
-                                            
+                                            <li
+                                                class="list-group-item card-header d-flex justify-content-between align-items-center">
+                                                "Task 1"
+                                                <div>
+                                                    <i class="fa-solid fa-pen-to-square"></i>
+                                                    <i class="fa-solid fa-trash"></i>
+                                                </div>
 
+                                            </li>
                                         </div>
                                         <div>
-                                          
+                                            <li
+                                                class="list-group-item card-header d-flex justify-content-between align-items-center">
+                                                "Task 1"
+                                                <div>
+                                                    <i class="fa-solid fa-pen-to-square"></i>
+                                                    <i class="fa-solid fa-trash"></i>
+                                                </div>
 
+                                            </li>
                                         </div>
                                         <div>
-                                           
+                                            <li
+                                                class="list-group-item card-header d-flex justify-content-between align-items-center">
+                                                "Task 1"
+                                                <div>
+                                                    <i class="fa-solid fa-pen-to-square"></i>
+                                                    <i class="fa-solid fa-trash"></i>
+                                                </div>
 
+                                            </li>
                                         </div>
                                     </ul>
-                                  </div>
+                                </div>
+                                <div class="card" style="width: 18rem;">
+                                    <div class="card-header d-flex justify-content-between">
+                                            <div>
+                                                <span>Completed</span>
+                                            </div>
+                                            <div>
+                                                <i class="fa-solid fa-plus " style="color: #769fcd;"></i>
+                                            </div>
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <div>
+                                            <li
+                                                class="list-group-item card-header d-flex justify-content-between align-items-center">
+                                                "Task 1"
+                                                <div>
+                                                    <i class="fa-solid fa-pen-to-square"></i>
+                                                    <i class="fa-solid fa-trash"></i>
+                                                </div>
+
+                                            </li>
+                                        </div>
+                                        <div>
+                                            <li
+                                                class="list-group-item card-header d-flex justify-content-between align-items-center">
+                                                "Task 1"
+                                                <div>
+                                                    <i class="fa-solid fa-pen-to-square"></i>
+                                                    <i class="fa-solid fa-trash"></i>
+                                                </div>
+
+                                            </li>
+                                        </div>
+                                        <div>
+                                            <li
+                                            class="list-group-item card-header d-flex justify-content-between align-items-center">
+                                            "Task 1"
+                                            <div>
+                                                <i class="fa-solid fa-pen-to-square"></i>
+                                                <i class="fa-solid fa-trash"></i>
+                                            </div>
+
+                                        </li>
+                                        </div>
+                                    </ul>
+                                </div>
                             </div>`
-
-
         })
     
